4986827f29bffc210b156dfa830a1f9a7f991094
pehff <pedrohf.fernandes@gmail.com>
2019-05-30 23:56:29 -0300

    Resolução do exercício 1005

---
+++ 1005/1005.py
@@ -0,0 +1,7 @@
+PESO_1 = 3.5
+PESO_2 = 7.5
+nota_a = float(input(''))
+nota_b = float(input(''))
+media = ((nota_a * PESO_1) + (nota_b * PESO_2)) / (PESO_1 + PESO_2)
+
+print('MEDIA = {:.5f}'.format(media))
