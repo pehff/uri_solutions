b542a9eb34b7ecf13a20321c79aa710b9866255f
pehff <pedrohf.fernandes@gmail.com>
2019-06-01 02:54:43 -0300

    Resolução do exercício 1006

---
+++ 1006/1006.py
@@ -0,0 +1,9 @@
+PESO_A = 2
+PESO_B = 3
+PESO_C = 5
+nota_a = float(input(''))
+nota_b = float(input(''))
+nota_c = float(input(''))
+media = ((nota_a * PESO_A) + (nota_b * PESO_B) + (nota_c * PESO_C)) / (PESO_A + PESO_B + PESO_C)
+
+print('MEDIA = {:.1f}'.format(media))
